6be21c243821014123a5fd4c60839e0fce62e187
qiurong <qiuqiu0113@163.com>
2015-12-31 15:50:59 +0800

    测试~~

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,21 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+  response.success("Hello world--qiurong!");
 });
+AV.Cloud.define("findUser",function(request,response){
+var userOId=request.params.userOId;
+var queryUser=new AV.Query('_User')
+queryUser.equalTo("objectId",userOId);
+queryUser.first({
+success:function(obj){
+
+console.log("---uu--"+JSON.stringify(obj));
+response.success(obj.get("username"));
+},error:function(errObj,error){
+console.log("------");
+}
+
+});
+
+})
